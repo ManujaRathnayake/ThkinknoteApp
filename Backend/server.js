c2542dcd11a8d8859834a9ab32564fe8bc7b2020
manuja Rathnayake <manujapawan619@gmail.com>
2026-06-22 09:27:54 +0530

    final level

--- Backend/server.js
+++ Backend/server.js
@@ -5,18 +5,25 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/ratelimiter.js';
 import cors from 'cors';
-
+import path from 'path';
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
+const _dirname = path.resolve()
 
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+       app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+})));
+}
 
 //middleware to parse JSON bodies
 app.use(express.json());
 app.use(rateLimiter)
-app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-}));
+
 
 //our custom middleware for logging requests
 // app.use((req, res, next) => {
@@ -26,6 +33,16 @@ app.use(cors({
 
 
 app.use("/api/notes" , router);
+app.use(express.static(path.join(_dirname,'Frountend/dist')))
+
+
+if(process.env.NODE_ENV === "Production"){
+        
+app.get("*" , (req,res)=>{
+    res.sendFile(path.join(_dirname,"../Frountend" , "dist" , "index.html"));
+})
+
+}
 
 connectDB().then(() => {
     console.log("Connected to MongoDB");
